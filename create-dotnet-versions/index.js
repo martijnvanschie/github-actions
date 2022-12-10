f3d05550f1cc83a849923783234b9acb7d8f6ae7
Martijn van Schie <martijnvanschie@gmail.com>
2022-12-10 10:13:29 +0100

    Fixed wrong files (OMG)

--- create-dotnet-versions/index.js
+++ create-dotnet-versions/index.js
@@ -3,22 +3,14 @@ const github = require('@actions/github');
 
 try {
   var baseVersion = core.getInput('version', {required: false}) || 'v1.0.0-beta.1';
-  core.debug(`Input version: ${baseVersion}`);
   core.info(`Input version: ${baseVersion}`);
-  core.warning(`Input version: ${baseVersion}`);
-  core.error(`Input version: ${baseVersion}`);
-
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-
-  core.info(github.context.payload.ref);
-  core.info(payload.ref);
-
-  const test = process.env.GITHUB_SHA || "GITHUB_SHA::undefined";
-  core.info(test);
 
   baseVersion = baseVersion.replace(/^(v)/,"");
   core.info(`Working version: ${baseVersion}`);
+
+  core.info(process.env.GITHUB_SHA);
+  const sha = process.env.GITHUB_SHA.substring(0,8);
+  core.info(`SHA: ${sha}`);
 
   core.info('Split versions on "."');
   const words = baseVersion.split('.');
@@ -27,7 +19,7 @@ try {
   });
   
   const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  core.setOutput("version-assembly", time);
 }
 catch (error) {
   core.setFailed(error.message);
