922c7ee1120010218ab97b7356d26e5ac31fc547
Martijn van Schie <martijnvanschie@gmail.com>
2022-12-10 16:58:43 +0100

    Updated code to generate all versions

--- create-dotnet-versions/dist/index.js
+++ create-dotnet-versions/dist/index.js
@@ -9695,17 +9695,34 @@ try {
   core.info(`Working version: ${baseVersion}`);
 
   core.info(process.env.GITHUB_SHA);
-  const sha = process.env.GITHUB_SHA.substring(0,8);
+  const sha = process.env.GITHUB_SHA ? process.env.GITHUB_SHA.substring(0,8) : "undefined";
   core.info(`SHA: ${sha}`);
 
   core.info('Split versions on "."');
-  const words = baseVersion.split('.');
-  words.forEach(element => {
-    core.info(element);
+  const versionParts = baseVersion.split('.');
+  versionParts.forEach(element => {
+    core.info(`Version part: ${element}`);
+  });
+
+  core.info('Split release on "-"');
+  const releaseParts = versionParts[2].split('-');
+  releaseParts.forEach(element => {
+    core.info(`Release part: ${element}`);
   });
   
-  const time = (new Date()).toTimeString();
-  core.setOutput("version-assembly", time);
+  const time = new Date();
+  const buildnumber = time.getHours() * 60 + time.getMinutes();
+
+  const assemblyVersion = `${versionParts[0]}.${versionParts[1]}`;
+  const fileVersion = `${versionParts[0]}.${versionParts[1]}.${releaseParts[0]}.${buildnumber}`;
+  const informationalVersion = `${baseVersion}+${sha}`;
+  const packageVersion = baseVersion;
+  
+  core.setOutput("version-assembly", assemblyVersion);
+  core.setOutput("version-file", fileVersion);
+  core.setOutput("version-informational", informationalVersion);
+  core.setOutput("version-package", packageVersion);
+  core.setOutput("buildnumber", buildnumber);
 }
 catch (error) {
   core.setFailed(error.message);
